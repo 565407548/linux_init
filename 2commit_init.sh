66d0ad2f19afab37481c32161dce5962f7fac20c
zcj <565407548@qq.com>
2015-05-21 10:28:26 +0800

    linux_init update

---
+++ 2commit_init.sh
@@ -0,0 +1,7 @@
+##linux_init
+cd ~/git/linux_init
+git remote add linux_init git@github.com:565407548/linux_init.git
+
+##algorithm
+cd ~/git/algorithm
+git remote add algorithm git@github.com:565407548/algorithm.git
---
+++ commit.sh
@@ -0,0 +1,13 @@
+##linux_init
+cd ~/git/linux_init
+git add ./*
+git commit -m "linux_init update"
+git push linux_init master
+
+##algorithm
+cd ~/git/algorithm
+git add ./*
+git commit -m "algorithm update"
+git push algorithm master
+
+##design_pattern
