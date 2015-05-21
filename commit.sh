3a96be6b7e5bbdd2a9faed56b260e289b80c0d5c
zcj <565407548@qq.com>
2015-05-21 19:40:12 +0800

    linux_init update

--- commit.sh
+++ commit.sh
@@ -1,12 +1,12 @@
 ##linux_init
 cd ~/git/linux_init
-git add ./*
+git add --all ./*
 git commit -m "linux_init update"
 git push linux_init master
 
 ##algorithm
 cd ~/git/algorithm
-git add ./*
+git add --all ./*
 git commit -m "algorithm update"
 git push algorithm master
 
