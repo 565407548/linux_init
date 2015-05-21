f3e1b289f708d215942bf8377ffcf4c0f4325d4b
zcj <565407548@qq.com>
2015-05-22 01:43:00 +0800

    linux_init update

--- commit_init.sh
+++ commit_init.sh
@@ -5,3 +5,7 @@ git remote add linux_init git@github.com:565407548/linux_init.git
 ##algorithm
 cd ~/git/algorithm
 git remote add algorithm git@github.com:565407548/algorithm.git
+
+##design_pattern
+cd ~/git/design_pattern
+git remote add design_pattern git@github.com:565407548/design_pattern.git
